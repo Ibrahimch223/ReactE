93f26cbb5c2549ab62c16e96b9f9015187a69617
haidara <haidaraibrahim1234@gmail.com>
2024-08-28 10:44:01 +0000

    tre push

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -3,6 +3,33 @@ import Logo from "../../assets/images/ETS.png";
 import ContryDrop from "../ContryDrown";
 import Button from "@mui/material/Button";
 import { IoMdSearch } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + "").replace(",", "").replace(" ", "");
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = typeof thousands_sep === "undefined" ? "," : thousands_sep,
+    dec = typeof dec_point === "undefined" ? "." : dec_point,
+    s = "",
+    toFixedFix = function (n, prec) {
+      var k = Math.pow(10, prec);
+      return "" + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split(".");
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || "").length < prec) {
+    s[1] = s[1] || "";
+    s[1] += new Array(prec - s[1].length + 1).join("0");
+  }
+  return s.join(dec);
+}
 
 const Header = () => {
   return (
@@ -35,6 +62,18 @@ const Header = () => {
                   <Button>
                     <IoMdSearch />
                   </Button>
+                </div>
+
+                <div className="part3 d-flex align-items-center ml-auto">
+                  <Button className="circle mr-3">
+                    <FaRegUser />
+                  </Button>
+                  <div className="ml-auto cartTab">
+                    <span className="price">5000CFA</span>
+                    <Button className="circle ml-2">
+                      <AiOutlineShoppingCart />
+                    </Button>
+                  </div>
                 </div>
               </div>
             </div>
