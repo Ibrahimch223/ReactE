73ed98890df7ff923215ab35d792937b16705661
haidara <haidaraibrahim1234@gmail.com>
2024-08-28 07:47:00 +0000

    tre push

---
+++ src/components/Headers/index.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="test">
+          <div className="container">
+            <p className=" text-center mb-0 mt-0">
+              Bienvenue Chez <b>ETS services</b> pour vos besoins electronique
+              et divers
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
