f62cebc9ce5409d044ef385dbdd9c08883ac318b
haidara <haidaraibrahim1234@gmail.com>
2024-08-28 08:15:05 +0000

    tre push

--- src/components/Headers/index.js
+++ src/components/Headers/index.js
@@ -1,13 +1,27 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/ETS.png";
 const Header = () => {
   return (
     <>
-      <div className="header">
+      <div className="headerw">
         <div className="test">
-          <div className="container">
+          <div className="containerw">
             <p className=" text-center mb-0 mt-0">
               Bienvenue Chez <b>ETS services</b> pour vos besoins electronique
               et divers
             </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="header">
+        <div className="container">
+          <div className="row">
+            <div className="logowapper col-sm-3">
+              <Link to={"/"}>
+                <img src={Logo} />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
