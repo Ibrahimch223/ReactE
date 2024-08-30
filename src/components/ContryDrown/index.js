d33f552c0bb12abaa4c2695dabe50c227c260305
haidara <haidaraibrahim1234@gmail.com>
2024-08-30 14:47:10 +0000

    ter

--- src/components/ContryDrown/index.js
+++ src/components/ContryDrown/index.js
@@ -2,11 +2,15 @@ import { FaAngleDown } from "react-icons/fa6";
 import Button from "@mui/material/Button";
 import { IoMdSearch } from "react-icons/io";
 import Dialog from "@mui/material/Dialog";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
 
 const ContryDrop = () => {
+  const [isOpenModal, setisOpenModal] = useState(false);
+
   return (
     <>
-      <Button className="contryDrop">
+      <Button className="contryDrop" onClick={() => setisOpenModal(true)}>
         <div className="info d-flex flex-column">
           <span className="label">Localisation</span>
           <span className="name">
@@ -17,9 +21,12 @@ const ContryDrop = () => {
           <FaAngleDown />
         </span>
       </Button>
-      <Dialog open={true} className="locationModal">
+      <Dialog open={isOpenModal} className="locationModal">
         <h4>Votre lieux</h4>
-        <div className="headersearch w-100">
+        <Button className="close" onClick={() => setisOpenModal(false)}>
+          <IoMdClose />
+        </Button>
+        <div className="headersearch mt-3 w-100">
           <input type="text" placeholder="lieux..." />
           <Button>
             <IoMdSearch />
